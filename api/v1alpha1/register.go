a11634026c6f3433f61a72f443a9cd1c4f0d4892
0xdeadc0de <0xdeadc0de@noreply.git.centerionware.com>
2026-05-12 21:53:40 +0000

    Update api/v1alpha1/register.go

--- api/v1alpha1/register.go
+++ api/v1alpha1/register.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -22,5 +23,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&PRDeployment{},
 		&PRDeploymentList{},
 	)
+	// Required for controller-runtime to convert ListOptions correctly
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
